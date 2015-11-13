231f5f63e795e4a6b7b2e552b19df1ae3f26e42a
Dmitry Abrosimov <d.abrosimov@axioma.lv>
2015-11-13 09:27:31 +0100

    Added FiltrationCompilerPassTest.

---
+++ Tests/DependencyInjection/Compiler/FiltrationCompilerPassTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Da2e\FiltrationBundle\Tests\DependencyInjection\Compiler;
+
+use Da2e\FiltrationBundle\DependencyInjection\Compiler\FiltrationCompilePass;
+use Da2e\FiltrationBundle\Tests\TestCase;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Class FiltrationCompilerPassTest
+ * @package Da2e\FiltrationBundle\Tests\DependencyInjection\Compiler
+ */
+class FiltrationCompilerPassTest extends TestCase
+{
+    public function testProcess_NoFilterChainFound()
+    {
+        $containerMock = $this->getCustomMock('\Symfony\Component\DependencyInjection\ContainerBuilder', [
+            'hasDefinition',
+            'getDefinition',
+            'findTaggedServiceIds',
+        ]);
+
+        $containerMock->expects($this->once())->method('hasDefinition')
+            ->with('da2e.filtration.filter.chain.filter_chain')
+            ->willReturn(false);
+
+        $containerMock->expects($this->never())->method('getDefinition');
+        $containerMock->expects($this->never())->method('findTaggedServiceIds');
+
+        $compilerPass = new FiltrationCompilePass();
+        $compilerPass->process($containerMock);
+    }
+
+    public function testProcess()
+    {
+        $containerMock = $this->getCustomMock('\Symfony\Component\DependencyInjection\ContainerBuilder', [
+            'hasDefinition',
+            'getDefinition',
+            'findTaggedServiceIds',
+        ]);
+
+        $containerMock->expects($this->once())->method('hasDefinition')
+            ->with('da2e.filtration.filter.chain.filter_chain')
+            ->willReturn(true);
+
+        $definitionMock = $this->getCustomMock('\stdClass', ['addMethodCall']);
+
+        $definitionMock->expects($this->at(0))->method('addMethodCall')
+            ->with('addType', [new Reference('foo'), 'foo1']);
+
+        $definitionMock->expects($this->at(1))->method('addMethodCall')
+            ->with('addType', [new Reference('baz'), 'baz1']);
+
+        $definitionMock->expects($this->at(2))->method('addMethodCall')
+            ->with('addType', [new Reference('bar'), 'bar1']);
+
+        $definitionMock->expects($this->exactly(3))->method('addMethodCall');
+
+        $containerMock->expects($this->once())->method('getDefinition')
+            ->with('da2e.filtration.filter.chain.filter_chain')
+            ->willReturn($definitionMock);
+
+        $containerMock->expects($this->once())->method('findTaggedServiceIds')
+            ->with('da2e.filtration.manager.filter')
+            ->willReturn([
+                'foo' => [['alias' => 'foo1']],
+                'baz' => [['alias' => 'baz1']],
+                'bar' => [['alias' => 'bar1']],
+            ]);
+
+        $compilerPass = new FiltrationCompilePass();
+        $compilerPass->process($containerMock);
+    }
+}
