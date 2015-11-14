18bdd6460b6929e044b9d31c642e4dc9e66f70db
Dmitry Abrosimov <d.abrosimov@axioma.lv>
2015-11-14 17:17:07 +0100

    Added TransformValuesFunctionValidatorTest.

--- CallableFunction/Validator/TransformValuesFunctionValidator.php
+++ CallableFunction/Validator/TransformValuesFunctionValidator.php
@@ -11,10 +11,7 @@ class TransformValuesFunctionValidator extends BaseFunctionValidator
     /**
      * {@inheritdoc}
      */
-    protected $argumentTypes = array(
-        // Abstract filter object.
-        0 => array(
-            'type' => 'Da2e\FiltrationBundle\Filter\Filter\FilterInterface'
-        )
-    );
+    protected $argumentTypes = [
+        ['type' => 'Da2e\FiltrationBundle\Filter\Filter\FilterInterface'],
+    ];
 }
---
+++ Tests/CallableFunction/Validator/TransformValuesFunctionValidatorTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Da2e\FiltrationBundle\Tests\CallableFunction\Validator;
+
+use Da2e\FiltrationBundle\CallableFunction\Validator\TransformValuesFunctionValidator;
+use Da2e\FiltrationBundle\Filter\Filter\AbstractFilter;
+use Da2e\FiltrationBundle\Filter\Filter\FilterInterface;
+use Da2e\FiltrationBundle\Tests\TestCase;
+
+/**
+ * Class TransformValuesFunctionValidatorTest
+ * @package Da2e\FiltrationBundle\Tests\CallableFunction\Validator
+ */
+class TransformValuesFunctionValidatorTest extends TestCase
+{
+    public function testProperty_ArgumentTypes()
+    {
+        $functionValidator = new TransformValuesFunctionValidator(function () {
+        });
+
+        $result = $this->getPrivateProperty($functionValidator, 'argumentTypes')->getValue($functionValidator);
+
+        $this->assertSame([
+            ['type' => 'Da2e\FiltrationBundle\Filter\Filter\FilterInterface'],
+        ], $result);
+    }
+
+    public function testIsValid()
+    {
+        $functionValidator = new TransformValuesFunctionValidator(function (FilterInterface $filter) {
+        });
+
+        $this->assertTrue($functionValidator->isValid());
+    }
+
+    public function testIsValid_False()
+    {
+        $functionValidator = new TransformValuesFunctionValidator(function (AbstractFilter $filter) {
+        });
+
+        $this->assertFalse($functionValidator->isValid());
+        $this->assertInstanceOf(
+            '\Da2e\FiltrationBundle\Exception\CallableFunction\Validator\CallableFunctionValidatorException',
+            $functionValidator->getException()
+        );
+    }
+}
