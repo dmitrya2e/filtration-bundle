eb253360e1ba993279ee6d330cbb6c85b76dfae9
Dmitry Abrosimov <d.abrosimov@axioma.lv>
2015-11-18 13:42:07 +0100

    Completed PHPDoc for FilterHandlerModel.

--- Model/FilterHandlerModel.php
+++ Model/FilterHandlerModel.php
@@ -1,11 +1,20 @@
 <?php
 
+/*
+ * This file is part of the Da2e FiltrationBundle package.
+ *
+ * (c) Dmitry Abrosimov <abrosimovs@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Da2e\FiltrationBundle\Model;
 
 /**
- * The class containing some static info/config about the filter handlers supported by the bundle.
+ * The class contains some static info/config about the filter handlers supported by the bundle.
  *
- * @package Da2e\FiltrationBundle\Model
+ * @author Dmitry Abrosimov <abrosimovs@gmail.com>
  */
 class FilterHandlerModel
 {
